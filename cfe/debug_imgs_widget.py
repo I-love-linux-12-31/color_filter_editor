5cfccf8d0042eba7261c9c9bfc110c52c01c5afb
yaroslav_admin <639e361476fb74a951435808@bots.bitbucket.org>
2024-02-20 14:05:29 +0300

    Fixed bug with wrong path of debug images.

--- cfe/debug_imgs_widget.py
+++ cfe/debug_imgs_widget.py
@@ -1,10 +1,16 @@
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QLabel, QWidget, QGridLayout
+import pathlib
+import sys
+import os
 
 
 class DebugImagesWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        app_exec = pathlib.Path(sys.argv[0])
+
         self.setMinimumSize(800, 300)
         self.gridLayout = QGridLayout(self)
         self.gridLayout.setObjectName(u"gridLayout")
@@ -12,7 +18,7 @@ class DebugImagesWindow(QWidget):
         self.label.setObjectName(u"gradient_img")
         self.label.setMinimumSize(512, 64)
 
-        pixmap = QPixmap("gradient_test.jpg")
+        pixmap = QPixmap(os.path.join(str(app_exec.parent), "gradient_test.jpg"))
         self.label.setPixmap(pixmap)
         self.label.setScaledContents(True)
 
@@ -24,7 +30,7 @@ class DebugImagesWindow(QWidget):
         self.label_2.setMaximumSize(512, 512)
         self.label_2.setScaledContents(True)
 
-        pixmap2 = QPixmap("color_debug.jpg")
+        pixmap2 = QPixmap(os.path.join(str(app_exec.parent), "color_debug.jpg"))
         self.label_2.setPixmap(pixmap2)
 
         self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
